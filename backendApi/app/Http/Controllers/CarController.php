852fc5101d75f25472c58ead88cc0adcf876cb9c
Mateusz Gajewski <mateusz.f.gajewski@gmail.com>
2020-09-17 12:52:26 +0200

    create Car controller

---
+++ backendApi/app/Http/Controllers/CarController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Car;
+use App\Http\Resources\Car as CarResource;
+use Illuminate\Http\Request;
+use App\Http\Requests;
+
+class CarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $cars = Car::paginate(15);
+
+        return CarResource::collection($cars);
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $car = $request->isMethod('put') ? Car::FindOrfail($request->car_id)
+         : new Car;
+
+        $car->id = $request->input('car_id');
+        $car->name = $request->input('name');
+        $car->brand = $request->input('brand');
+        $car->model = $request->input('model');
+        $car->description = $request->input('description');
+        $car->rented = 0;
+        $car->renter = '';
+        if($car->save()){
+            
+            return new CarResource($car);
+        }
+
+    }
+
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $car = Car::findOrFail($id);
+
+        return new CarResource($car);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $car = Car::findOrFail($id);
+
+        if($car->delete()){
+            return new CarResource($car);
+        }
+
+    }
+}
