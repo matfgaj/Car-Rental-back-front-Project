9e7f40a93e173ab6a4d3e13f6cb2b29a2eb9088f
Mateusz Gajewski <mateusz.f.gajewski@gmail.com>
2020-09-17 12:52:12 +0200

    update route

--- backendApi/routes/api.php
+++ backendApi/routes/api.php
@@ -2,12 +2,19 @@
 
 use Illuminate\Http\Request;
 
+// Auth routes
 Route::group(['prefix' => 'auth', 'namespace' => 'Auth'], function (){
     Route::post('signin', 'SignInController');
     Route::post('signout', 'SignOutController');
     Route::get('me', 'MeController');
     Route::get('login', [ 'as' => 'login', 'uses' => 'SignInController']);
-
-
 });
+// Cars routes
+Route::get('cars', 'CarController@index');
+Route::get('car/{id}', 'CarController@show');
+Route::post('car', 'CarController@store');
+Route::put('car', 'CarController@store');
+Route::delete('car/{id}', 'CarController@destroy');
+
+
 
