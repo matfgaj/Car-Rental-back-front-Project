0b2d2ac6e087d8a5c0c5e1d6f0ad4c38c6eb5a47
Mateusz Gajewski <mateusz.f.gajewski@gmail.com>
2020-09-18 12:41:30 +0200

    update car

--- backendApi/database/factories/CarFactory.php
+++ backendApi/database/factories/CarFactory.php
@@ -5,9 +5,10 @@ use Faker\Generator as Faker;
 $factory->define(App\Car::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
-        'brand'=> $faker->text(7),
+        'brand'=> $faker->randomElement(["Skoda","Ford","BMW","Fiat"]),
         'model'=> $faker->text(7),
         'description'=> $faker->text(100),
+        'productionyear'=> $faker->randomElement([2017,2015,2013,2011]),
         'rented'=> $faker->boolean('false'),
         'renter'=> $faker->randomElement(['','']),
     ];
