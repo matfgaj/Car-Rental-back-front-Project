487133941fe7209c1192392fbe9c19b842b2b1de
Mateusz Gajewski <mateusz.f.gajewski@gmail.com>
2020-09-19 21:58:00 +0200

    update car model

--- backendApi/app/Car.php
+++ backendApi/app/Car.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Car extends Model
 {
-    // use HasFactory;
+// use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'brand', 'model', 'productionyear','description',
+    ];
 }
