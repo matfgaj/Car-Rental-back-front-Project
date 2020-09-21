d055d34f9be8c7dd46a6ff8db7d4c21dcfbd6a87
Mateusz Gajewski <mateusz.f.gajewski@gmail.com>
2020-09-21 13:45:36 +0200

    create sign in view and work on sign in/out

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,14 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
+import axios from "axios";
 
-Vue.config.productionTip = false
+require("@/store/subscriber");
 
-new Vue({
-  router,
-  store,
-  vuetify,
-  render: h => h(App)
-}).$mount('#app')
+axios.defaults.baseURL = "http://127.0.0.1:8000/api/";
+
+Vue.config.productionTip = false;
+
+store.dispatch("auth/attempt", localStorage.getItem("token")).then(() => {
+  new Vue({
+    router,
+    store,
+    vuetify,
+    render: (h) => h(App),
+  }).$mount("#app");
+});
---
+++ frontend/src/views/SignIn.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-container class="fill-height" fluid>
+    <v-row align="center" justify="center">
+      <v-col cols="12" sm="8" md="4">
+        <v-card class="elevation-12">
+          <v-toolbar color="grey" dark flat>
+            <v-toolbar-title>Welcome</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-icon @click="home">mdi-close</v-icon>
+          </v-toolbar>
+          <v-card-text>
+            <v-form>
+              <v-text-field
+                label="email"
+                name="email"
+                prepend-icon="mdi-account"
+                type="text"
+                v-model="form.email"
+              >
+              </v-text-field>
+              <v-text-field
+                label="Password"
+                name="password"
+                prepend-icon="mdi-lock"
+                type="text"
+                v-model="form.password"
+              ></v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn @click="submit" dark color="grey">Login</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+export default {
+  name: "signin",
+  components: {
+    //
+  },
+  data() {
+    return {
+      form: {
+        email: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    ...mapActions({
+      signIn: "auth/signIn",
+    }),
+    submit() {
+      this.signIn(this.form).then(() => {
+        this.$router.replace({
+          name: "profile",
+        });
+      });
+    },
+    home() {
+      this.$router.replace({ path: "/" });
+    },
+  },
+};
+</script>
