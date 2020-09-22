a0cb91f3244f065c4547b8d7c8a9a756d9b94a10
Mateusz Gajewski <mateusz.f.gajewski@gmail.com>
2020-09-22 17:48:28 +0200

    make home view

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,65 @@
 <template>
-  <div>home</div>
+  <v-container fluid grid-list-xl>
+    <v-layout wrap justify-space-around>
+      <v-flex v-for="car in carsList" :key="car.id" xs12 sm6 md4>
+        <v-card>
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
+            aspect-ratio="2.75"
+            ><v-container fluid>
+              <v-layout>
+                <v-flex xs12 align-end flexbox>
+                  <span class="headline white--text">{{ car.name }}</span>
+                </v-flex>
+              </v-layout>
+            </v-container></v-img
+          >
+          <v-card-title primary-title>
+            <v-card-text small
+              >{{ car.description.slice(0, 100) }}...</v-card-text
+            >
+          </v-card-title>
+          <v-card-actions>
+            <v-btn color="orange">Details</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
+import { mapActions } from "vuex";
+import axios from "axios";
+
 export default {
   name: "Home",
-  components: {},
   data() {
     return {
       initialized: false,
+      carsList: null,
+      paginationLinks: null,
+      displayedPage: null,
     };
   },
+  methods: {
+    ...mapActions({
+      signIn: "home/getCarsList",
+    }),
+    async getFirtsCarsList() {
+      if (!this.carsList) {
+        let response = await axios.get("cars?page=1");
+        if (response) {
+          this.carsList = response.data.data;
+          console.log(this.carsList);
+          this.paginationLinks = response.data.links;
+          console.log(this.paginationLinks);
+        }
+      }
+    },
+  },
   created() {
-    this.fetchData().then(() => {
+    this.getFirtsCarsList().then(() => {
       this.initialized = true;
     });
   },
