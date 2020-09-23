f6ba66907271dbcb5a7fd29e8f8ff1db5c04d03b
Mateusz Gajewski <mateusz.f.gajewski@gmail.com>
2020-09-24 01:09:25 +0200

    import cars

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,11 +1,15 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import auth from "./auth";
+import cars from "./cars";
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  namespaced: true,
+
   state: {},
   mutations: {},
   actions: {},
-  modules: { auth },
+  modules: { auth, cars },
 });
