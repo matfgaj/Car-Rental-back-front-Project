ad6ed3386937f13ef832eff9f03b854be97f6145
Mateusz Gajewski <mateusz.f.gajewski@gmail.com>
2020-09-23 14:42:04 +0200

    update factory

--- backendApi/database/factories/CarFactory.php
+++ backendApi/database/factories/CarFactory.php
@@ -7,7 +7,6 @@ $factory->define(App\Car::class, function (Faker $faker) {
         'name' => $faker->name,
         'brand'=> $faker->randomElement(["Skoda","Ford","BMW","Fiat"]),
         'model'=> $faker->text(7),
-        'image'=> $faker->text(7),
         'description'=> $faker->text(100),
         'productionyear'=> $faker->randomElement([2017,2015,2013,2011]),
         'rented'=> $faker->boolean('false'),
