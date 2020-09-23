b4270feed374a7088412afee6d8bcdcdf6c092df
Mateusz Gajewski <mateusz.f.gajewski@gmail.com>
2020-09-24 01:12:32 +0200

    update component

--- frontend/src/views/Profile.vue
+++ frontend/src/views/Profile.vue
@@ -1,14 +1,29 @@
-<template>
-  <div>
-    Profile
+<template
+  ><div>
+    <AdminProfile v-if="role === 'admin'"> </AdminProfile>
+    <UserProfile v-if="role === 'user'"></UserProfile>
   </div>
 </template>
 
 <script>
+import AdminProfile from "@/components/AdminProfile";
+import UserProfile from "@/components/UserProfile";
+
 export default {
   name: "Profile",
   components: {
-    //
+    AdminProfile,
+    UserProfile,
+  },
+  data() {
+    return {
+      role: this.$store.state.auth.user.role,
+    };
+  },
+  methods: {
+    test() {
+      console.log(this.$store.state.auth.user.role);
+    },
   },
 };
 </script>
