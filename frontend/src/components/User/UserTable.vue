9a6b649848f24e87511414b10477550b45eb4cb3
Mateusz Gajewski <mateusz.f.gajewski@gmail.com>
2020-09-27 23:33:51 +0200

    create user table

---
+++ frontend/src/components/User/UserTable.vue
@@ -0,0 +1,39 @@
+<template
+  ><div>
+    <v-data-table
+      class="elevation-1"
+      :headers="headers"
+      :items="getcarList"
+    ></v-data-table>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  data() {
+    return {
+      headers: [
+        {
+          text: "brand",
+          value: "brand",
+        },
+        { text: "model", value: "model" },
+        { text: "Production year", value: "productionyear" },
+        { text: "Description", value: "description" },
+      ],
+    };
+  },
+  watch: {
+    getcarList() {
+      console.log("ja");
+    },
+  },
+  computed: {
+    ...mapGetters({ getcarList: "user/getcarList" }),
+  },
+  created() {
+    this.getcarList;
+  },
+};
+</script>
