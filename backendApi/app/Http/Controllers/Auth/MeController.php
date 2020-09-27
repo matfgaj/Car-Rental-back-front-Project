a2df880649a48c07762b9eff1c1a6f4cec735a5b
Mateusz Gajewski <mateusz.f.gajewski@gmail.com>
2020-09-27 23:26:07 +0200

    add user

--- backendApi/app/Http/Controllers/Auth/MeController.php
+++ backendApi/app/Http/Controllers/Auth/MeController.php
@@ -17,7 +17,8 @@ class MeController extends Controller
     {
         $user = $request->user();
 
-        return response()->json([
+        return response()->json([            
+            'id' => $user->id,
             'email' => $user->email,
             'name' => $user->name,
             'role' => $user->role,
