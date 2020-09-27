72e1fdcacad8d52a9db7ed6af90a3f04bab53814
Mateusz Gajewski <mateusz.f.gajewski@gmail.com>
2020-09-27 23:31:05 +0200

    add  rent function

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,7 +1,9 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
 import auth from "./auth";
 import cars from "./cars";
+import user from "./user";
 
 Vue.use(Vuex);
 
@@ -10,6 +12,23 @@ export default new Vuex.Store({
 
   state: {},
   mutations: {},
-  actions: {},
-  modules: { auth, cars },
+  actions: {
+    async PostUserRent({ state }, car) {
+      if (state.auth.user.cars === "[]") {
+        let newArray = JSON.stringify([car]);
+
+        axios.put(`user?user_id=${state.auth.user.id}&cars=${newArray}`);
+        const response = await axios.post(`car/${car.id}/rent`);
+        return response;
+      } else {
+        let carsArray = JSON.parse(state.auth.user.cars);
+        let newcarsArray = [...carsArray, car];
+        const arrayToSend = JSON.stringify(newcarsArray);
+        axios.put(`user?user_id=${state.auth.user.id}&cars=${arrayToSend}`);
+        const response = await axios.post(`car/${car.id}/rent`);
+        return response;
+      }
+    },
+  },
+  modules: { auth, cars, user },
 });
