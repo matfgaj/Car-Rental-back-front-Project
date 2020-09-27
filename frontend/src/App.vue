47e2c7821b11b463ae882b7abdc8e55ef2c296e6
Mateusz Gajewski <mateusz.f.gajewski@gmail.com>
2020-09-27 23:28:24 +0200

    add footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,6 +2,13 @@
   <v-app>
     <Navigation></Navigation>
     <router-view></router-view>'
+    <v-flex>
+      <v-footer absolute padless class="font-weight-medium">
+        <v-col class="text-center" cols="12">
+          {{ new Date().getFullYear() }} — <strong>Car Rent</strong>
+        </v-col>
+      </v-footer>
+    </v-flex>
   </v-app>
 </template>
 <script>
